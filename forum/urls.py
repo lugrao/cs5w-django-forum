70eab1044c436764d46f4f34a4b4337bb8f13d62
e <elugrao@gmail.com>
2021-12-07 12:35:41 -0300

    add Topic url

--- forum/urls.py
+++ forum/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register"),
+    path("topic/<str:topic_name>", views.topic, name="topic"),
 ]
